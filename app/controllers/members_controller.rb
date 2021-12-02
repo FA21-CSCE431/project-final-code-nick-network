bf6ec8327dbe39574754c6203c22b36eaa60eca9
adribwadud <adribwadud@gmail.com>
2021-12-01 21:15:01 -0600

    Made members alphabetical

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,7 +6,7 @@ class MembersController < ApplicationController
 
   # GET /members or /members.json
   def index
-    @members = Member.all
+    @members = Member.all.order(:name)
   end
 
   # GET /members/1 or /members/1.json

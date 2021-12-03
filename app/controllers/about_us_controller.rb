52f42fc44d22ce69d792434ae772e86eaaf33601
ChristianRossKane <christian.kane@hotmail.com>
2021-12-03 14:13:04 -0600

    nic (admin) credentials no longer hardcoded

--- app/controllers/about_us_controller.rb
+++ app/controllers/about_us_controller.rb
@@ -2,4 +2,14 @@
 
 class AboutUsController < ApplicationController
   def Index; end
+
+  def execute_statement(sql)
+    results = ActiveRecord::Base.connection.execute(sql)
+
+    if results.present?
+      return results
+    else
+      return nil
+    end
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def execute_statement(sql)
+    results = ActiveRecord::Base.connection.execute(sql)
+
+    if results.present?
+      return results
+    else
+      return nil
+    end
+  end
+
 end
